db4b8916a20ba16d08617cc9d309870d50ec93ab
newtontush <newtontush@gmail.com>
2019-02-22 13:59:00 +0300

    Add question 6 to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
         var val3 = parseInt($("[name=answer3]:checked").val());
         var val4 = parseInt($("[name=answer4]:checked").val());
         var val5 = parseInt($("[name=answer5]:checked").val());
-        $("#input").text("Your score is: " + (val1 + val2 + val3 + val4 + val5));
+        var val6 = parseInt($("[name=answer6]:checked").val());
+        $("#input").text("Your score is: " + (val1 + val2 + val3 + val4 + val5 + val6));
     });
 });

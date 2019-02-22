c64baaaf42042c54585c223ae2a7d40a2b07a938
newtontush <newtontush@gmail.com>
2019-02-22 17:13:11 +0300

    Add images to image folder

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,10 @@ $(document).ready(function() {
         var val9 = parseInt($("[name=answer9]:checked").val());
         var val10 = parseInt($("[name=answer10]:checked").val());
         var result = parseInt(val1+ val2 + val3 + val4 + val5 + val6 + val7 + val8 + val9 + val10 );
-         $("#input").text("Your score is: " + result);
+        if (result > 50) {
+         $("#input").text("CONGRATULATIONS" + "Your score is: " + result);
+        }else{
+            $("#input").text( "Your score is: " + result + "You will have to retake the exam");   
+        }
     });
 });

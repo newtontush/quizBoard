57c45701ef9e86bc5912af78c8c7f8e41f692f0a
newtontush <newtontush@gmail.com>
2019-02-22 12:34:42 +0300

    Add second question to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $("#questions").submit(function(event) {
         event.preventDefault();
         var val1 = parseInt($("[name=answer1]:checked").val());
-        $("#input").text( val1);
+        var val2 = parseInt($("[name=answer2]:checked").val());
+        $("#input").text("Your score is: " + (val1 + val2));
     });
 });

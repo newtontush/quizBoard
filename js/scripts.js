bdf52d94c47a907861678a6498c2b6eabe79ee43
newtontush <newtontush@gmail.com>
2019-02-22 12:59:00 +0300

    Add question 5 to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
         var val1 = parseInt($("[name=answer1]:checked").val());
         var val2 = parseInt($("[name=answer2]:checked").val());
         var val3 = parseInt($("[name=answer3]:checked").val());
-        $("#input").text("Your score is: " + (val1 + val2 + val3));
+        var val4 = parseInt($("[name=answer4]:checked").val());
+        var val5 = parseInt($("[name=answer5]:checked").val());
+        $("#input").text("Your score is: " + (val1 + val2 + val3 + val4 + val5));
     });
 });
